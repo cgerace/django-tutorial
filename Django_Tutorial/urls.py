0c77d3110cbeea125232bf33d1cf8d918cbcdea3
Christian Gerace <cgerace8@gmail.com>
2020-01-07 17:02:20 -0500

    Completed part 7 of tutorial, modifying admin page

--- Django_Tutorial/urls.py
+++ Django_Tutorial/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+admin.site.site_header = 'Polls Admin'
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
